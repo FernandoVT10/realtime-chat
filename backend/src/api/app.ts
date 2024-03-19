c35aaa001ba07ad0a64f246a9f7f6a24c1728e0a
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-03-18 19:14:28 -0600

    Add cors for now

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -2,11 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes";
 import errorHandler from "./middlewares/errorHandler";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 
 app.use("/api", routes);
 app.use(errorHandler);
