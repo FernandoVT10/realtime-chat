c3fe7fb3b9d2fd38bc80710f462f71b5bc9cb767
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-04-01 19:23:49 -0600

    Make the search case insensitive

--- backend/src/api/services/UsersService.ts
+++ backend/src/api/services/UsersService.ts
@@ -4,7 +4,7 @@ import type { UserProfile } from "@types";
 const USERS_LIMIT = 10;
 
 const getUsersByUsernameSearch = (search: string): Promise<UserProfile[]> => {
-  const regex = new RegExp(search);
+  const regex = new RegExp(search, "i");
 
   return UserModel.find<UserProfile>({
     username: { $regex: regex },
