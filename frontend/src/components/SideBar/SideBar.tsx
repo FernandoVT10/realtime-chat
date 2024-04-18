c6feadb8792d2fb299bfaa55212b88e87ea0d10b
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-04-17 18:49:34 -0600

    Adding styles...

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -6,7 +6,10 @@ import UserAvatar from "./UserAvatar";
 import AddFriendModal from "./AddFriendModal";
 import UserInfo from "./UserInfo";
 import PendingRequests from "./PendingRequests";
+import Spinner from "../Spinner"
 import axiosInstance from "../../axios";
+import getFirstErrorMessage from "../../utils/getFirstErrorMessage";
+import classNames from "classnames";
 
 import styles from "./SideBar.module.scss";
 
@@ -16,6 +19,9 @@ interface SideBarProps {
 
 function SideBar({ user }: SideBarProps) {
   const [friends, setFriends] = useState<UserProfile[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
   const addFriendModal = useModal();
   const pendingRequestsModal = useModal();
 
@@ -26,41 +32,83 @@ function SideBar({ user }: SideBarProps) {
 
         setFriends(res.data.friends);
       } catch (error) {
-        // TODO: handle this error correctly
-        console.error(error);
+        setError(getFirstErrorMessage(error));
       }
+
+      setLoading(false);
     };
 
     getFriends();
   }, []);
+
+  const getFriendsComponent = () => {
+    if(loading) {
+      return (
+        <div className={styles.statusContainer}>
+          <Spinner size={30}/>
+        </div>
+      );
+    }
+
+    if(error) {
+      return (
+        <div className={styles.statusContainer}>
+          <span className={styles.errorMessage}>{ error }</span>
+        </div>
+      );
+    }
+
+    if(!friends.length) {
+      // TODO: Add a SVG instead
+      return (
+        <div className={styles.statusContainer}>
+          <span className={styles.infoMessage}>You don't have friends :(</span>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <h3 className={styles.subtitle}>Friends</h3>
+
+        <div className={styles.friends}>
+          {friends.map(friend => {
+            return (
+              <div className={styles.friend} key={friend._id}>
+                <UserAvatar avatar={friend.avatar} status={{ isOnline: true }} />
+                <span className={styles.username}>{friend.username}</span>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className={styles.sideBar}>
       <AddFriendModal modal={addFriendModal}/>
       <PendingRequests modal={pendingRequestsModal}/>
 
-      <div>
-        <button type="button" onClick={pendingRequestsModal.showModal}>
-          Pending friend requests
+      <div className={styles.buttonsContainer}>
+        <button
+          type="button"
+          onClick={pendingRequestsModal.showModal}
+          className={classNames(styles.button, "custom-submit-button")}
+        >
+          Pending friends requests
         </button>
 
-        <button type="button" onClick={addFriendModal.showModal}>
+        <button
+          type="button"
+          onClick={addFriendModal.showModal}
+          className={classNames(styles.button, "custom-submit-button")}
+        >
           Add a new friend
         </button>
       </div>
-      
-      <h3 className={styles.subtitle}>Friends</h3>
 
-      <div className={styles.friends}>
-        {friends.map(friend => {
-          return (
-            <div className={styles.friend} key={friend._id}>
-              <UserAvatar avatar={friend.avatar} status={{ isOnline: true }} />
-              <span className={styles.username}>{friend.username}</span>
-            </div>
-          );
-        })}
-      </div>
+      {getFriendsComponent()}
 
       <UserInfo
         avatar={user.avatar}
