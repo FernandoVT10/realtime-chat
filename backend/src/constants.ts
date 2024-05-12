0470218147a58be1d3ce68fd9e7f9c55595211de
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-05-11 18:15:26 -0600

    Make nginx server the uploaded images instead of express.js

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,8 +1,7 @@
-import path from "path";
 import { config } from "dotenv";
 config();
 
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
-export const UPLOADS_DIRECTORY = path.join(__dirname, "../uploads/");
+export const UPLOADS_DIRECTORY = "/uploads/";
 
 export const MONGO_URI = process.env.MONGO_URI as string;
